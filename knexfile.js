caed608f9599cd21021f083caa80735a38ef972d
Kevin Springer <KevinUSCU@outlook.com>
2018-01-18 14:25:10 -0800

    Add knex configuration.

---
+++ knexfile.js
@@ -0,0 +1,37 @@
+// Set knex environment
+const path = require('path')
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/quoi_dev',
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/quoi_test',
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.PRODUCTION_DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  }
+}
