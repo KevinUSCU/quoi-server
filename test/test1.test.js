0fec1ec73282b19d54b7d2bbf51c571a239aa80b
Kevin Springer <KevinUSCU@outlook.com>
2018-01-18 14:26:10 -0800

    Begin to add test framework.

---
+++ test/test1.test.js
@@ -0,0 +1,13 @@
+// const model = require('../models/XXXXX.model')
+
+describe('Basic Test', () => {
+
+  describe('Base', () => {
+    test('It should...', () => {
+      console.log(process.env.NODE_ENV)
+      console.log(process.env.DATABASE_URL)
+      expect(true).toBe(true)
+    })
+  })
+
+})
