d61b808fc643952c4311fc78ae75195162ed3922
Kevin Springer <KevinUSCU@outlook.com>
2018-01-22 09:47:04 -0800

    Add active_today column to tips table.

--- db/migrations/20180120163159_tips.js
+++ db/migrations/20180120163159_tips.js
@@ -2,6 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('tips', (table) => {
     table.increments()
     table.string('tip').notNullable().defaultTo('')
+    table.boolean('active_today').notNullable().defaultTo(false)
     table.boolean('used').notNullable().defaultTo(false)
     table.timestamps(true, true)
   })
