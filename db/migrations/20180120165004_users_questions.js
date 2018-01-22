ce4f6b09687e63ef85586ec12b09805d5a1ed14c
Kevin Springer <KevinUSCU@outlook.com>
2018-01-22 09:48:03 -0800

    Add table.

--- db/migrations/20180120165004_users_questions.js
+++ db/migrations/20180120165004_users_questions.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex, Promise) {
-  
-};
+  return knex.schema.createTable('users_questions', (table) => {
+    table.increments()
+    table.integer('user_id').notNullable()
+    table.foreign('user_id').references('users.id')
+    table.integer('question_id').notNullable()
+    table.foreign('question_id').references('questions.id')
+    table.string('answer_history').notNullable().defaultTo('')
+    table.boolean('considers_relevant').notNullable()
+    table.timestamps(true, true)
+  })
+}
 
 exports.down = function(knex, Promise) {
-  
-};
+  return knex.schema.dropTable('users_questions')
+}
---
+++ db/migrations/20180122092614_users_daily_questions.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users_daily_questions', (table) => {
+    table.increments()
+    table.integer('user_id').notNullable()
+    table.foreign('user_id').references('users.id')
+    table.integer('daily_question_id').notNullable()
+    table.foreign('daily_question_id').references('daily_questions.id')
+    table.boolean('got_correct').notNullable()
+    table.timestamps(true, true)
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users_daily_questions')
+}
