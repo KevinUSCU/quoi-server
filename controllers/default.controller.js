002fe52e1551a1fa78eff2b6696a0b2401f28bd3
Kevin Springer <KevinUSCU@outlook.com>
2018-01-23 12:31:10 -0800

    Add default controller.

--- controllers/default.controller.js
+++ controllers/default.controller.js
@@ -12,35 +12,34 @@ module.exports = name => {
     }
 
     static index (req, res, next) {
-      Model.all(req.params.userId)
+      Model.all()
       .then(response => res.status(200).json({ [`${name}s`]: response }))
       .catch(next)
     }
 
     static show (req, res, next) {
-      if (!Number(req.params.id)) throw new Error(`noSuchRoute`) //catch malformed join routes
-      Model.find(req.params.id, req.params.userId)
+      Model.find(req.params.id)
       .then(response => {
-        if (!response) throw new Error(`noSuch${name}`) //redundant if using 'exists' first in route chain
+        if (!response) throw new Error(`noSuch${name}`)
         return res.status(200).json({ [name]: response })
       })
       .catch(next)
     }
 
     static create (req, res, next) {
-      Model.create(req.body, req.params.userId)
+      Model.create(req.body)
       .then(response => res.status(201).json({ [name]: response }))
       .catch(next)
     }
 
     static update (req, res, next) {
-      Model.update(req.params.id, req.body, req.params.userId)
+      Model.update(req.params.id, req.body)
       .then(response => res.status(200).json({ [name]: response }))
       .catch(next)
     }
 
     static destroy (req, res, next) {
-      Model.destroy(req.params.id, req.params.userId)
+      Model.destroy(req.params.id)
       .then(response => res.status(204).json()) //no response body with 204
       .catch(next)
     }
