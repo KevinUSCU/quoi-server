448d8fcf77128d6badee54da7a5b31c6e2076ea1
Kevin Springer <KevinUSCU@outlook.com>
2018-01-23 00:08:22 -0800

    Add tests for find.

--- test/auth.model.test.js
+++ test/auth.model.test.js
@@ -23,7 +23,17 @@ describe('Auth Model', () => {
 
   describe('View all', () => {
     test('It should throw an error if .all() method is called', () => {
-      return expect(() => model.all()).toThrow()
+      expect(() => model.all()).toThrow()
+    })
+  })
+
+  describe('Find', () => {
+    test('It should return a user when a valid email is provided', () => {
+      const user = { user_id: 1, email: 'admin@email.com', hashed_password: '$2a$10$x5BXBKwhjo.VCl8A/tuFfuLfEv8narDxPbvx6yMHlG.Y6EreIcCFa' }
+      return expect(model.find('admin@email.com')).resolves.toMatchObject(user)
+    })
+    test('It should return undefined when an invalid email is provided', () => {
+      return expect(model.find('notPresent')).resolves.toBe(undefined)
     })
   })
 
