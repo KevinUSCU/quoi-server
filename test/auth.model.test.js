bed593ae940eae84a851aa2b1044adee717205e5
Kevin Springer <KevinUSCU@outlook.com>
2018-01-23 00:41:45 -0800

    Add tests for Update and Destroy.

--- test/auth.model.test.js
+++ test/auth.model.test.js
@@ -33,9 +33,35 @@ describe('Auth Model', () => {
       return expect(model.find('admin@email.com')).resolves.toMatchObject(user)
     })
     test('It should return undefined when an invalid email is provided', () => {
-      return expect(model.find('notPresent')).resolves.toBe(null)
+      return expect(model.find('notPresent')).resolves.toBe(undefined)
     })
   })
 
+  describe('Update', () => {
+    const email = 'newuser@email.com'
+    const hashedPassword = 'changedhash'
+    const user1 = { user_id: 1, email: 'newuser@email.com', hashed_password: 'changedhash' }
+    const user2 = { user_id: 2, email: 'newuser@email.com', hashed_password: '$2a$10$x5BXBKwhjo.VCl8A/tuFfuLfEv8narDxPbvx6yMHlG.Y6EreIcCFa' }
+    const user3 = { user_id: 3, email: 'john@email.com', hashed_password: 'changedhash' }
+
+    test('It should return an updated user when both new email and hashed_passwords are provided', () => {
+      return expect(model.update(1, { email, hashed_password: hashedPassword })).resolves.toMatchObject(user1)
+    })
+    test('It should return an updated user when only a new email is provided', () => {
+      return expect(model.update(2, { email })).resolves.toMatchObject(user2)
+    })
+    test('It should return an updated user when only a new password is provided', () => {
+      return expect(model.update(3, { hashed_password: hashedPassword })).resolves.toMatchObject(user3)
+    })
+    test('It should return undefined when an invalid userId is provided', () => {
+      return expect(model.update(7, { email, hashed_password: hashedPassword })).resolves.toBe(undefined)
+    })
+  })
+
+  describe('Destroy', () => {
+    test('It should throw an error if .destroy() method is called', () => {
+      expect(() => model.destroy(1)).toThrow()
+    })
+  })
 
 })
