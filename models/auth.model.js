325f713c265e618db49d0a1c7bb1fa447fd93fc4
Kevin Springer <KevinUSCU@outlook.com>
2018-01-23 00:10:08 -0800

    Add auth model.

---
+++ models/auth.model.js
@@ -0,0 +1,31 @@
+const Model = require('./default.model')('auth') //auth is the table name
+const db = require('../db/knex')
+
+class AuthModel extends Model {
+
+  //returning all password hashes seems risky, so we pre-emptively override and disallow this
+  static all () {
+    throw new Error('Requesting all entries is not allowed per security policy')
+  }
+
+  static find (email) {
+    return db('auth')
+    .where({ email })
+    .first()
+  }
+
+  static update (userId, body) {
+    return db('auth')
+      .where({ user_id: userId })
+      .update({ ...body, thisKeyIsSkipped: undefined })
+      .returning('*')
+      .then(([ result ]) => result)
+  }
+
+  //auth table entry will be destroyed automatically when user is deleted
+  static destroy (userId) {
+    throw new Error('You should not call "destroy" on auth; deleting a user will delete this entry as required')
+  }
+}
+
+module.exports = AuthModel
