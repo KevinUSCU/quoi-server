ea23967e94e6a98a35e2e96181ed42ca2932a255
Kevin Springer <KevinUSCU@outlook.com>
2018-01-23 11:43:21 -0800

    Add Auth, Token, and User Models.

--- models/index.js
+++ models/index.js
@@ -1,3 +1,6 @@
 module.exports = {
-  ErrorModel: require('./error.model.js')
+  AuthModel: require('./auth.model'),
+  ErrorModel: require('./error.model'),
+  TokenModel: require('./token.model'),
+  UserModel: require('./user.model')
 }
