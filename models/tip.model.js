02bd6876baa228e97a43aa5797c48d9ee71b89f8
Kevin Springer <KevinUSCU@outlook.com>
2018-01-24 00:17:17 -0800

    Add randomNewTipOfTheDay and resetTips methods.

--- models/tip.model.js
+++ models/tip.model.js
@@ -1,6 +1,30 @@
 const Model = require('./default.model')('tips') //tips is the table name
 const db = require('../db/knex')
 
-class TipsModel extends Model {}
+class TipModel extends Model {
 
-module.exports = TipsModel
+  static randomNewTipOfTheDay() {
+    return db('tips')
+      .where({ used: false })
+    .then(tips => {
+      if (tips.length === 0) return TipModel.resetTips()
+      else return tips
+    })
+    .then(tips => {
+      const randomIndex = Math.floor(Math.random() * tips.length)
+      const tip = tips[randomIndex]
+      super.update(tip.id, { used: true })
+      return tip.tip
+    })
+  }
+
+  static resetTips() {
+    return db('tips')
+    .where({ used: true })
+    .update({ used: false })
+    .returning('*')
+  }
+
+}
+
+module.exports = TipModel
