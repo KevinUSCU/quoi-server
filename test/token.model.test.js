f057b48b5741adc42eff4d3198657f7a5ad60500
Kevin Springer <KevinUSCU@outlook.com>
2018-01-28 01:03:58 -0800

    Fix test broken by authorization header capitalization change.

--- test/token.model.test.js
+++ test/token.model.test.js
@@ -7,7 +7,7 @@ describe('Token Model', () => {
   test('It should be able to sign a token and then decode that token to get the same value it started with', () => {
     return expect((async () => {
       const token = await model.sign(1)
-      const header = { Authorization: `Bearer ${token}` }
+      const header = { authorization: `Bearer ${token}` }
       const result = await model.verifyAndExtractHeaderToken(header)
       return result.sub.id
     })())
