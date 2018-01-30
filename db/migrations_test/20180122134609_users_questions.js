89ff3a516959ca0584edc07accfd075e321da1b1
Kevin Springer <KevinUSCU@outlook.com>
2018-01-30 15:32:28 -0800

    Cascade delete when User is deleted.

--- db/migrations_test/20180122134609_users_questions.js
+++ db/migrations_test/20180122134609_users_questions.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('users_questions', (table) => {
     table.increments()
     table.integer('user_id').notNullable()
-    table.foreign('user_id').references('users.id')
+    table.foreign('user_id').references('users.id').onDelete('CASCADE')
     table.integer('question_id').notNullable()
     table.foreign('question_id').references('questions.id')
     table.string('answer_history').notNullable().defaultTo('')
--- db/migrations_test/20180122134631_users_daily_questions.js
+++ db/migrations_test/20180122134631_users_daily_questions.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('users_daily_questions', (table) => {
     table.increments()
     table.integer('user_id').notNullable()
-    table.foreign('user_id').references('users.id')
+    table.foreign('user_id').references('users.id').onDelete('CASCADE')
     table.integer('daily_question_id').notNullable()
     table.foreign('daily_question_id').references('daily_questions.id')
     table.boolean('got_correct').notNullable()
