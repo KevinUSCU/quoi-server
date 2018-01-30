7a9ca3d8f3246ba71c47c5c6e9c7dfc051502820
Kevin Springer <KevinUSCU@outlook.com>
2018-01-30 12:02:21 -0800

    Add findMatch method.

--- models/user-question.model.js
+++ models/user-question.model.js
@@ -1,6 +1,14 @@
 const Model = require('./default.model')('users_questions') // users_questions is the table name
 const db = require('../db/knex')
 
-class UserQuestionModel extends Model {}
+class UserQuestionModel extends Model {
+
+  static findMatch(userId, questionId) {
+    return db('users_questions')
+    .where({ user_id: userId, question_id: questionId})
+    .first() // We will gurantee we've never created more than one 
+  }
+
+}
 
 module.exports = UserQuestionModel
