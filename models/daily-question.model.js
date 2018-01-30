cd8722ba7ac1f3f29e7e694c139ae87fd0154344
Kevin Springer <KevinUSCU@outlook.com>
2018-01-30 14:30:27 -0800

    Add findByDate method.

--- models/daily-question.model.js
+++ models/daily-question.model.js
@@ -1,6 +1,14 @@
 const Model = require('./default.model')('daily_questions') //daily_questions is the table name
 const db = require('../db/knex')
 
-class DailyQuestionModel extends Model {}
+class DailyQuestionModel extends Model {
+
+  static findByDate(date) {
+    return db('daily_questions')
+    .where({ date })
+    .first() // There should only be one entry for each date
+  }
+
+}
 
 module.exports = DailyQuestionModel
