9a126c82d003fa039814f7894ea76358f4dbb4e9
Kevin Springer <KevinUSCU@outlook.com>
2018-01-30 23:22:07 -0800

    Change name of function to be more descriptive (now getServerDayOfTheWeek).

--- routes/date.routes.js
+++ routes/date.routes.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const { DateController } = require(`../controllers`)
 
-router.get('/', DateController.getServerDate)
+router.get('/', DateController.getServerDayOfTheWeek)
 
 module.exports = router
