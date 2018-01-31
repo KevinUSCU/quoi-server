0e4dc9ae6e69dcf49300a2df0067728885b82971
Kevin Springer <KevinUSCU@outlook.com>
2018-01-30 23:21:17 -0800

    Add getAllDailyQuestionsForUser method.

--- models/user-daily-question.model.js
+++ models/user-daily-question.model.js
@@ -1,6 +1,15 @@
 const Model = require('./default.model')('users_daily_questions') // users_daily_questions is the table name
 const db = require('../db/knex')
 
-class UserDailyQuestionModel extends Model {}
+class UserDailyQuestionModel extends Model {
+
+  static getAllDailyQuestionsForUser(userId) {
+    return db('users_daily_questions')
+    .select('date', 'question_id', 'got_correct')
+    .where({ user_id: userId })
+    .join('daily_questions', 'daily_question_id', 'daily_questions.id')
+  }
+
+}
 
 module.exports = UserDailyQuestionModel
