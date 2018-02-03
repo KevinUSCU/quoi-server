132e1669351db7144d2364beaa02b5c085917b31
Kevin Springer <KevinUSCU@outlook.com>
2018-02-03 00:09:06 -0800

    Modify tests for Travis.

--- test/setup.js
+++ test/setup.js
@@ -1,20 +1,20 @@
 //Recreate test database before tests
 
 module.exports = () => {
-  console.log(process.env)
-  const connection = require('knex')
-  const dbName = 'quoi_test'
+  if (!process.env.TRAVIS) { // Don't run this on Travis CI
+    const connection = require('knex')
+    const dbName = 'quoi_test'
 
-  let db = connection({
-    client: 'pg',
-    connection: 'postgres://localhost'
-  })
+    let db = connection({
+      client: 'pg',
+      connection: 'postgres://localhost'
+    })
 
-  return db.raw(`DROP DATABASE IF EXISTS ${dbName};`)
-  .then(result => db.raw(`CREATE DATABASE ${dbName};`))
-  .then(result => db.destroy())
-  .catch((err) => {
-    throw new Error(err)
-  })
-
+    return db.raw(`DROP DATABASE IF EXISTS ${dbName};`)
+    .then(result => db.raw(`CREATE DATABASE ${dbName};`))
+    .then(result => db.destroy())
+    .catch((err) => {
+      throw new Error(err)
+    })
+  }
 }
--- test/teardown.js
+++ test/teardown.js
@@ -1,18 +1,19 @@
 // //Remove the database at the end of testing
 
 module.exports = () => {
-  const connection = require('knex')
-  const dbName = 'quoi_test'
+  if (!process.env.TRAVIS) { // Don't run this on Travis CI
+    const connection = require('knex')
+    const dbName = 'quoi_test'
 
-  let db = connection({
-    client: 'pg',
-    connection: 'postgres://localhost'
-  })
+    let db = connection({
+      client: 'pg',
+      connection: 'postgres://localhost'
+    })
 
-  return db.raw(`DROP DATABASE IF EXISTS ${dbName};`)
-  .then(result => db.destroy())
-  .catch((err) => {
-    throw new Error(err)
-  })
-
+    return db.raw(`DROP DATABASE IF EXISTS ${dbName};`)
+    .then(result => db.destroy())
+    .catch((err) => {
+      throw new Error(err)
+    })
+  }
 }
