f317ba74938bdedefdbfccd18068385d656664ef
Kevin Springer <KevinUSCU@outlook.com>
2018-02-03 13:37:51 -0800

    Add tests for daily-question.model.

---
+++ test/daily-question.model.test.js
@@ -0,0 +1,40 @@
+const model = require('../models/daily-question.model')
+const db = require('../db/knex')
+
+describe('Daily Question Model', () => {
+
+  beforeAll(() => {
+    return db.migrate.latest()
+    .catch(err => {
+      throw new Error(err)
+    })
+  })
+
+  beforeEach(() => {
+    return db.seed.run()
+    .catch(err => {
+      throw new Error(err)
+    })
+  })
+
+  afterAll(() => {
+    return db.destroy()
+    .catch(err => {
+      throw new Error(err)
+    })
+  })
+
+  describe('Find (by date)', () => {
+    test('It should return the daily question for an existing date', () => {
+      const dataEntry = { date: '2000-01-01T05:15:50.118Z', question_id: 1 }
+      return model.create(dataEntry)
+      .then(() => {
+        return expect(model.findByDate('2000-01-01T05:15:50.118Z')).resolves.toMatchObject(dataEntry)
+      })
+    })
+    test('It should return undefined when there is no daily question for the given date', () => {
+      return expect(model.findByDate('notPresent')).resolves.toBe(undefined)
+    })
+  })
+
+})
