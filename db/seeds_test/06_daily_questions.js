1dafdcdf8430e1ac1369d06abcc014538017e8fc
Kevin Springer <KevinUSCU@outlook.com>
2018-02-03 18:13:19 -0800

    Test for another Travis fix.

--- db/seeds_test/06_daily_questions.js
+++ db/seeds_test/06_daily_questions.js
@@ -1,9 +1,9 @@
-exports.seed = function(knex, Promise) {
-  return knex('daily_questions').insert([
-    { id: 1, date: '2018-01-27T05:15:50.118Z', question_id: 2 },
-    { id: 2, date: '2018-01-28T05:15:50.118Z', question_id: 1 }
-  ])
-  .then(() => {
-    return knex.raw(`SELECT setval('daily_questions_id_seq', (SELECT MAX(id) FROM daily_questions));`)
-  })
-}
+// exports.seed = function(knex, Promise) {
+//   return knex('daily_questions').insert([
+//     { id: 1, date: '2018-01-27T05:15:50.118Z', question_id: 2 },
+//     { id: 2, date: '2018-01-28T05:15:50.118Z', question_id: 1 }
+//   ])
+//   .then(() => {
+//     return knex.raw(`SELECT setval('daily_questions_id_seq', (SELECT MAX(id) FROM daily_questions));`)
+//   })
+// }
