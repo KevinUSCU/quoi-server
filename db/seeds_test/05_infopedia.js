3d72b7e0375f16b11220ef55da3373b3091bd6df
Kevin Springer <KevinUSCU@outlook.com>
2018-02-04 01:05:45 -0800

    edit and add seeds.

--- db/seeds_test/05_infopedia.js
+++ db/seeds_test/05_infopedia.js
@@ -3,20 +3,26 @@ exports.seed = function(knex, Promise) {
     { 
       id: 1,
       category: 'Functions',
-      title: 'Function declaration',
-      description: 'A function declaration in JavaScript follows the format: function x(y) { return y + 1 }.',
+      title: 'Declaration',
+      description: 'A function declaration in JavaScript follows the format:\n\n    function x(y) {\n        return y + 1\n    }',
     },
     { 
       id: 2,
       category: 'Functions',
-      title: 'Function expression',
-      description: 'A function expression in JavaScript follows the format: const x = function(y) { return y + 1 }.',
+      title: 'Expression',
+      description: 'A function expression in JavaScript follows the format:\n\n    const x = function(y) {\n        return y + 1\n    }',
     },
     { 
       id: 3,
-      category: 'Arrays',
+      category: 'Objects',
       title: 'Array',
       description: 'An array is a global object in JavaScript which contains values ordered by an index number starting at 0.',
+    },
+    {
+      id: 4,
+      category: 'Code Execution',
+      title: 'Short-Circuit Evaluation',
+      description: "Short-circuit evaluation in a programming language means that a Boolean operation will only evaluate the second condition if the first condition is not sufficient to determine the value of the expression.\n\nFor example, when using the && operator, if the first condition evaluates as 'false', the second condition will be ignored as the entire expression is now guaranteed to be 'false.' The same is true for the || operator if the first condition evaluates to be 'true.'"
     }
     
   ])
