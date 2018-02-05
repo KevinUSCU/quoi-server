853745dd127f26f8797e44ba44f82dacbfe58131
Kevin Springer <KevinUSCU@outlook.com>
2018-02-04 22:12:29 -0800

    Update seeds for demo.

--- db/seeds_test/02_users.js
+++ db/seeds_test/02_users.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex, Promise) {
   return knex('users').insert([
-    { id: 1, firstname: 'Admin', lastname: 'Istrator', role: 'admin' },
+    { id: 1, firstname: 'Kevin', lastname: 'Admin', role: 'admin' },
     { id: 2, firstname: 'Lorem', lastname: 'Ipsum' },
     { id: 3, firstname: 'John', lastname: 'Doe' },
     { id: 4, firstname: 'Jane', lastname: 'Grey' }
